073d665765f50a7577eed46c4eddb57ee08f1b74
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-04-02 18:07:50 +0200

    Change names to pythonic names

--- graphData.py
+++ graphData.py
@@ -6,116 +6,116 @@ import os
 import sys
 
 class GraphData():
-    def __init__(self, filesNamesToGraph):
-        self.gaussianType, self.uniformType = "GAUSS", "UNIFORM"
-        self.randomTypes = [self.gaussianType, self.uniformType]
-        self.filesNames = filesNamesToGraph
-        self.dataNames = [os.path.splitext(self.filesNames[i])[0] for i in range(len(self.filesNames))]
-        self.dataList = []
-        self.dataMeanValues = []
-        self.dataMaxValues = []
-        self.dataMinValues = []
-        self.dataStandardDeviations = []
-        self.randomData = []
-        self.plots = [None for i in range(len(self.filesNames))]
-        self.meanVertical = None
-        self.XValues = []
+    def __init__(self, files_names_to_graph):
+        self.gaussian_type, self.uniform_type = "GAUSS", "UNIFORM"
+        self.random_types = [self.gaussian_type, self.uniform_type]
+        self.files_names = files_names_to_graph
+        self.data_names = [os.path.splitext(self.files_names[i])[0] for i in range(len(self.files_names))]
+        self.data_list = []
+        self.data_mean_values = []
+        self.data_max_values = []
+        self.data_min_values = []
+        self.data_standard_deviations = []
+        self.random_data = []
+        self.plots = [None for i in range(len(self.files_names))]
+        self.mean_vertical = None
+        self.x_values = []
 
-    def fileOperations(self):
-        for i in range(len(self.filesNames)):
-            f = open(self.filesNames[i])
-            self.dataList.append(json.load(f))
+    def file_operations(self):
+        for i in range(len(self.files_names)):
+            f = open(self.files_names[i])
+            self.data_list.append(json.load(f))
             f.close
 
-    def dataPreperations(self):
-        self.meanVertical = round((self.maxValue(self.dataList) + self.minValue(self.dataList))/2)
-        for i in range(len(self.dataList)):
-            self.XValues.append(list(range(1, len(self.dataList[i]) + 1)))
-            self.dataMeanValues.append(mean(self.dataList[i]))
-            self.dataStandardDeviations.append(stdev(self.dataList[i]))
-            self.dataMaxValues.append([max(self.dataList[i]), self.dataList[i].index(max(self.dataList[i]))])
-            self.dataMinValues.append([min(self.dataList[i]), self.dataList[i].index(min(self.dataList[i]))])
+    def data_preperations(self):
+        self.mean_vertical = round((self.max_value(self.data_list) + self.min_value(self.data_list))/2)
+        for i in range(len(self.data_list)):
+            self.x_values.append(list(range(1, len(self.data_list[i]) + 1)))
+            self.data_mean_values.append(mean(self.data_list[i]))
+            self.data_standard_deviations.append(stdev(self.data_list[i]))
+            self.data_max_values.append([max(self.data_list[i]), self.data_list[i].index(max(self.data_list[i]))])
+            self.data_min_values.append([min(self.data_list[i]), self.data_list[i].index(min(self.data_list[i]))])
 
-    def maxValue(self, inputList):
-        return max([max(list) for list in inputList])
+    def max_value(self, input_list):
+        return max([max(list) for list in input_list])
 
-    def minValue(self, inputList):
-        return min([min(list) for list in inputList])
+    def min_value(self, input_list):
+        return min([min(list) for list in input_list])
 
-    def prepareHistograms(self):
+    def prepare_histograms(self):
         fig = plt.figure("Histograms Figure")
-        for i in range(len(self.dataList)):
-            subplotNumber = len(self.dataList)*100 + 10 + i + 1
-            self.plots[i] = fig.add_subplot(subplotNumber)
-            self.plots[i].hist(self.dataList[i])
-            self.plots[i].set_title(self.dataNames[i] + " Histogram")
+        for i in range(len(self.data_list)):
+            subplot_number = len(self.data_list)*100 + 10 + i + 1
+            self.plots[i] = fig.add_subplot(subplot_number)
+            self.plots[i].hist(self.data_list[i])
+            self.plots[i].set_title(self.data_names[i] + " Histogram")
 
-    def generateRandomMeasurement(self, randomType):
-        if randomType == self.gaussianType:
-            for i in range(len(self.dataList)):
-                self.randomData.append(round(random.uniform(min(self.dataList[i]), max(self.dataList[i]))))
-        elif randomType == self.uniformType:
-            for i in range(len(self.dataList)):
-                self.randomData.append(round(random.gauss(self.dataMeanValues[i], self.dataStandardDeviations[i])))
+    def generate_random_measurement(self, random_type):
+        if random_type == self.uniform_type:
+            for i in range(len(self.data_list)):
+                self.random_data.append(round(random.uniform(min(self.data_list[i]), max(self.data_list[i]))))
+        elif random_type == self.gaussian_type:
+            for i in range(len(self.data_list)):
+                self.random_data.append(round(random.gauss(self.data_mean_values[i], self.data_standard_deviations[i])))
 
-    def prepareValuesGraph(self):
+    def prepare_values_graph(self):
         plt.figure("All Data Figure")
         # Add values from files to plot
-        for i in range(len(self.dataList)):
-            plt.plot(self.XValues[i], self.dataList[i], label = self.dataNames[i], marker="o")
-            plt.text(len(self.dataList[i]) + 1, self.dataList[i][len(self.dataList[i]) - 1], str(self.dataList[i][len(self.dataList[i]) - 1]))
+        for i in range(len(self.data_list)):
+            plt.plot(self.x_values[i], self.data_list[i], label = self.data_names[i], marker="o")
+            plt.text(len(self.data_list[i]) + 1, self.data_list[i][len(self.data_list[i]) - 1], str(self.data_list[i][len(self.data_list[i]) - 1]))
         # Add mean line to plot
-        for i in range(len(self.dataList)):
-            plt.plot([0, len(self.dataList[i])], [self.dataMeanValues[i], self.dataMeanValues[i]], label = self.dataNames[i] + " mean value")
+        for i in range(len(self.data_list)):
+            plt.plot([0, len(self.data_list[i])], [self.data_mean_values[i], self.data_mean_values[i]], label = self.data_names[i] + " mean value")
         # Add random values to plot
-        for type in self.randomTypes:
-            self.generateRandomMeasurement(type)
-            stringToPrint = "Random " + str(type.lower()) + " data: "
-            for i in range(len(self.dataList)):
-                stringToPrint += self.dataNames[i] + " - " + str(self.randomData[i]) + "; "
-            print(stringToPrint)
-            for i in range(len(self.dataList)):
-                plt.plot(len(self.dataList[i]) + 5, self.randomData[i], label = "Random " + str(type.lower()) + " " + self.dataNames[i],   linestyle='dashed', marker= "*", markersize=5)
-                plt.text(len(self.dataList[i]) + 5, self.randomData[i], str(self.randomData[i]), horizontalalignment='left')
-            self.randomData.clear()
+        for type in self.random_types:
+            self.generate_random_measurement(type)
+            string_to_print = "Random " + str(type.lower()) + " data: "
+            for i in range(len(self.data_list)):
+                string_to_print += self.data_names[i] + " - " + str(self.random_data[i]) + "; "
+            print(string_to_print)
+            for i in range(len(self.data_list)):
+                plt.plot(len(self.data_list[i]) + 5, self.random_data[i], label = "Random " + str(type.lower()) + " " + self.data_names[i], linestyle='dashed', marker= "*", markersize=5)
+                plt.text(len(self.data_list[i]) + 5, self.random_data[i], str(self.random_data[i]), horizontalalignment='left')
+            self.random_data.clear()
         # Add mean values text to plot
-        stringToPrint = "Mean values: "
-        for i in range(len(self.dataList)):
-            stringToPrint += str(self.dataNames[i]) + " - " + str(round(self.dataMeanValues[i])) + "; "
+        string_to_print = "Mean values: "
+        for i in range(len(self.data_list)):
+            string_to_print += str(self.data_names[i]) + " - " + str(round(self.data_mean_values[i])) + "; "
         # Add standard deviations text to plot
-        stringToPrint += "Standard deviations: "
-        for i in range(len(self.dataList)):
-            stringToPrint += str(self.dataNames[i]) + " - " + str(round(self.dataStandardDeviations[i], 2)) + "; "
-        maxXValuesLength = max([len(xValues) for xValues in self.XValues])
-        plt.text(round(maxXValuesLength/2), self.meanVertical, stringToPrint, horizontalalignment='center')
+        string_to_print += "Standard deviations: "
+        for i in range(len(self.data_list)):
+            string_to_print += str(self.data_names[i]) + " - " + str(round(self.data_standard_deviations[i], 2)) + "; "
+        max_x_values_length = max([len(x_values) for x_values in self.x_values])
+        plt.text(round(max_x_values_length/2), self.mean_vertical, string_to_print, horizontalalignment='center')
         # Add min, max values to the plot
-        for i in range(len(self.dataList)):
-            if self.dataMaxValues[i][0] != len(self.dataList[i]) - 1:
-                plt.text(self.dataMaxValues[i][1] + 1, self.dataMaxValues[i][0], "Max - " + str(self.dataMaxValues[i][0]), verticalalignment="bottom", horizontalalignment="center")
-            if self.dataMinValues[i][0] != len(self.dataList[i]) - 1:
-                plt.text(self.dataMinValues[i][1] + 1, self.dataMinValues[i][0] - 2, "Min - " + str(self.dataMinValues[i][0]), verticalalignment="bottom", horizontalalignment="center")
+        for i in range(len(self.data_list)):
+            if self.data_max_values[i][0] != len(self.data_list[i]) - 1:
+                plt.text(self.data_max_values[i][1] + 1, self.data_max_values[i][0], "Max - " + str(self.data_max_values[i][0]), verticalalignment="bottom", horizontalalignment="center")
+            if self.data_min_values[i][0] != len(self.data_list[i]) - 1:
+                plt.text(self.data_min_values[i][1] + 1, self.data_min_values[i][0] - 2, "Min - " + str(self.data_min_values[i][0]), verticalalignment="bottom", horizontalalignment="center")
 
-    def plotAll(self):
+    def plot_all(self):
         plt.xlabel("x - samples")
-        stringToPrint = "y - measurement ("
-        for i in range(len(self.dataList)):
-            if i != len(self.dataNames) - 1:
-                stringToPrint += self.dataNames[i] + ", "
+        string_to_print = "y - measurement ("
+        for i in range(len(self.data_list)):
+            if i != len(self.data_names) - 1:
+                string_to_print += self.data_names[i] + ", "
             else:
-                stringToPrint += self.dataNames[i] + ")"
-        plt.ylabel(stringToPrint)
+                string_to_print += self.data_names[i] + ")"
+        plt.ylabel(string_to_print)
         plt.title("Measurements")
         plt.legend(loc='center left', framealpha=0.5)
         plt.show()
 
     def run(self):
-        self.fileOperations()
-        self.dataPreperations()
-        self.prepareHistograms()
-        self.prepareValuesGraph()
-        self.plotAll()
+        self.file_operations()
+        self.data_preperations()
+        self.prepare_histograms()
+        self.prepare_values_graph()
+        self.plot_all()
 
-def showInstruction():
+def show_instruction():
     print("Program for graphing data read from selected files.")
     print("Files to read are selected by writing their extensions in the terminal (If multiple file extensions will be used, they should be written as a ... (no idea for now))")
     print("For now, data can't be load with corresponding x values - only the measurements.")
@@ -123,19 +123,19 @@ def showInstruction():
     print("y names of values are determined by names of the corresponding files.\n")
 
 if __name__ == '__main__':
-    showInstruction()
-    amountOfFilesWithGivenExtention = 0
-    while amountOfFilesWithGivenExtention == 0:
-        fileExtention = input('Specify extention of files to be included in the graph (e.g.: txt, json - without "."): ').lower()
-        filesNames = [file for file in os.listdir() if file.endswith("." + fileExtention)]
-        amountOfFilesWithGivenExtention = len(filesNames)
-        if amountOfFilesWithGivenExtention == 0:
-            print("No ." + fileExtention + " files found in directory: '" + str(os.path.abspath(os.getcwd())) + "'.")
+    show_instruction()
+    amount_of_files_with_given_extention = 0
+    while amount_of_files_with_given_extention == 0:
+        file_extention = input('Specify extention of files to be included in the graph (e.g.: txt, json - without "."): ').lower()
+        files_names = [file for file in os.listdir() if file.endswith("." + file_extention)]
+        amount_of_files_with_given_extention = len(files_names)
+        if amount_of_files_with_given_extention == 0:
+            print("No ." + file_extention + " files found in directory: '" + str(os.path.abspath(os.getcwd())) + "'.")
             print("Files found: '", end = "")
-            filesToPrint = "', '".join(os.listdir())
-            print(filesToPrint + "'\nTry again.")
+            files_to_print = "', '".join(os.listdir())
+            print(files_to_print + "'\nTry again.")
             sys.exit()
         else:
-            print(str(amountOfFilesWithGivenExtention) + ' files with "' + fileExtention + '" extention found.')
-    measurementsDataGraph = GraphData(filesNames)
-    measurementsDataGraph.run()
+            print(str(amount_of_files_with_given_extention) + ' files with "' + file_extention + '" extention found.')
+    measurements_data_graph = GraphData(files_names)
+    measurements_data_graph.run()
